48d8a0c848b853bf0607c9b917416b3899fc01ef
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-08-30 11:51:23 +0530

    use chrome webdriver and time for sleep

--- first_selenium.py
+++ first_selenium.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
-driver = webdriver.Chrome()
+service = Service(r"webdrivers\chromedriver-win64\chromedriver.exe")
+
+driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.google.com")
 
+time.sleep(2)
+
 print(driver.title)
+
+time.sleep(2)
 
 driver.quit()
