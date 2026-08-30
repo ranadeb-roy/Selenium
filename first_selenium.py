51f6da580b0db5f19cb5066b0747478fa6de0c17
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-08-30 12:17:01 +0530

    find an element using NAME locator & send_keys to type some text

--- first_selenium.py
+++ first_selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+from selenium.webdriver.chrome.service import Service   # used to start a webdriver (e.g. chrome webdriver)
+from selenium.webdriver.common.by import By     # used for locators (i.e. ID, CLASS, etc.)
+import time     # used to stop the script for a specific amount of time using time.sleep()
 
 service = Service(r"webdrivers\chromedriver-win64\chromedriver.exe")
 
@@ -13,19 +14,23 @@ time.sleep(2)
 print("Title: ", driver.title)
 print("URL: ", driver.current_url)
 
-driver.get("https://www.youtube.com")   # search youtube
-time.sleep(2)
+search_box = driver.find_element(By.NAME, "q")  # find the search box using NAME "q"
+search_box.send_keys("Selenium Python")         # type "Selenium Python" in the search box
+time.sleep(5)
 
-print("Title: ", driver.title)
-print("URL: ", driver.current_url)
+# driver.get("https://www.youtube.com")   # search youtube
+# time.sleep(2)
 
-driver.back()   # go back to google
-time.sleep(2)
+# print("Title: ", driver.title)
+# print("URL: ", driver.current_url)
 
-driver.forward()    # go forward to youtube
-time.sleep(2)
+# driver.back()   # go back to google
+# time.sleep(2)
 
-driver.refresh()    # refresh youtube
-time.sleep(2)
+# driver.forward()    # go forward to youtube
+# time.sleep(2)
+
+# driver.refresh()    # refresh youtube
+# time.sleep(2)
 
 driver.quit()
