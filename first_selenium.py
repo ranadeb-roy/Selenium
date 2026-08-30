d57e26b84aff2bfdb89e6e02392082b098f1a6a7
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-08-30 12:22:03 +0530

    using Keys to press a key from keyboard

--- first_selenium.py
+++ first_selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service   # used to start a webdriver (e.g. chrome webdriver)
 from selenium.webdriver.common.by import By     # used for locators (i.e. ID, CLASS, etc.)
+from selenium.webdriver.common.keys import Keys     # used to click keys from the keyboard
 import time     # used to stop the script for a specific amount of time using time.sleep()
 
 service = Service(r"webdrivers\chromedriver-win64\chromedriver.exe")
@@ -16,6 +17,8 @@ print("URL: ", driver.current_url)
 
 search_box = driver.find_element(By.NAME, "q")  # find the search box using NAME "q"
 search_box.send_keys("Selenium Python")         # type "Selenium Python" in the search box
+time.sleep(1)
+search_box.send_keys(Keys.ENTER)                # clicks ENTER after typing
 time.sleep(5)
 
 # driver.get("https://www.youtube.com")   # search youtube
