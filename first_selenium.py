7aad639dd88600a6ba71cd7885944e7b4c2d32b4
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-08-30 12:00:37 +0530

    maximize_window() | get() | sleep() | title | current_url | back() | forward() | refresh() | quit()

--- first_selenium.py
+++ first_selenium.py
@@ -5,13 +5,27 @@ import time
 service = Service(r"webdrivers\chromedriver-win64\chromedriver.exe")
 
 driver = webdriver.Chrome(service=service)
+driver.maximize_window()    # full screen
 
-driver.get("https://www.google.com")
-
+driver.get("https://www.google.com")    # search google
 time.sleep(2)
 
-print(driver.title)
+print("Title: ", driver.title)
+print("URL: ", driver.current_url)
 
+driver.get("https://www.youtube.com")   # search youtube
+time.sleep(2)
+
+print("Title: ", driver.title)
+print("URL: ", driver.current_url)
+
+driver.back()   # go back to google
+time.sleep(2)
+
+driver.forward()    # go forward to youtube
+time.sleep(2)
+
+driver.refresh()    # refresh youtube
 time.sleep(2)
 
 driver.quit()
