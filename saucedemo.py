e46af497b61a1188ab28c950f577251b7bc2312f
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-08-30 15:17:54 +0530

    saucedemo part 1 - accessing elements & entering data

---
+++ saucedemo.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+driver_loc = r"webdrivers\chromedriver-win64\chromedriver.exe"
+service = Service(driver_loc)
+
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+driver.get("https://www.saucedemo.com/")
+time.sleep(2)
+
+username = driver.find_element(By.ID, "user-name")
+username.send_keys("standard_user")
+time.sleep(2)
+
+password = driver.find_element(By.ID, "password")
+password.send_keys("secret_sauce")
+time.sleep(2)
+
+driver.quit()
