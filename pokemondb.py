217687d860fd8751f50b3a645d558ae3f2af5223
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-09-07 00:19:22 +0530

    scrape pokemon data and print 10 pokemon data

--- pokemondb.py
+++ pokemondb.py
@@ -12,7 +12,7 @@ driver = webdriver.Edge()
 # Navigate to the Pokemon Database website
 driver.get("https://pokemondb.net/")
 driver.maximize_window()
-print("\n", driver.title, "\n")
+print(driver.title, "\n")
 time.sleep(2)
 
 # Navigate to "Pokemon FireRed & LeafGreen" page
@@ -20,7 +20,6 @@ games_menu = driver.find_element(By.XPATH, "/html/body/nav/ul/li[3]/a")
 ActionChains(driver).move_to_element(games_menu).perform()
 time.sleep(2)
 driver.find_element(By.LINK_TEXT, "FireRed & LeafGreen").click()
-time.sleep(2)
 
 # Wait until the page is fully loaded
 wait = WebDriverWait(driver, 30)    # Wait for max 30 seconds
@@ -32,6 +31,27 @@ time.sleep(2)
 driver.find_element(By.LINK_TEXT, "FireRed & LeafGreen Pokédex").click()
 wait.until(lambda driver: driver.execute_script("return document.readyState") == "complete")
 print(driver.title, "\n")
+time.sleep(2)
+
+# Scrape the data of 10 Pokemons in the Pokedex
+info_card = driver.find_elements(By.CLASS_NAME, "infocard")
+print(f"Found {len(info_card)} Pokemons in the Pokedex.\n")
+
+pokemon_data = []
+for card in info_card:
+    id = card.find_element(By.XPATH, "./span[2]/small[1]").text
+    name = card.find_element(By.XPATH, "./span[2]/a").text
+    types = card.find_element(By.XPATH, "./span[2]/small[2]").text
+    pokemon_data.append({"id": id, "name": name, "types": types})
+
+print("First 10 Pokemons in the Pokedex:")
+for i in range(10):
+    data = pokemon_data[i]
+    print(f"Id: {data['id']}")
+    print(f"Name: {data['name']}")
+    print(f"Types: {data['types']}")
+    print("---")
+
 time.sleep(2)
 
 driver.quit()
